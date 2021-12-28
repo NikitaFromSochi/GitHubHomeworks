e985c96c7e9bc301407819d7d09de659d37c88c3
NikitaFromSochi <96183401+NikitaFromSochi@users.noreply.github.com>
2021-12-28 21:42:57 +0300

    Second Homework with flags problem

--- GeekBrainsSecondHomework.cpp
+++ GeekBrainsSecondHomework.cpp
@@ -4,12 +4,27 @@
 #include <iostream>
 #include <locale.h>
 
+struct Game // Новый тип данных игра
+{
+    unsigned short size; // размер поля
+    unsigned short difficulty; // сложность игры
+    double points; // количество очков в случае победы
+};
+
+union MyVariant // моя кастомная структура
+{
+    int BotIq; // АйКью бота
+    float MyMultiplier; // Множитель очков в зависимости от сложности
+    char MyPin; // возможность выбора другого знака вместо Х
+    
+};
+
 int main()
 {
     setlocale(LC_ALL, "Russian");
     
     // short int, int, long long, char, bool, float, double
-        short int First = 3270;
+    short int First = 3270;
     int Second = 2345678;
     long long Third = -9876543211234;
     char Fourth = 'b';
@@ -19,13 +34,36 @@ int main()
     std::cout << First << " , " << Second << " , " << Third << " , " << Fourth << " , " << Fifth << " , " << Sixth << " , " << Seventh << std::endl;
 
    
-    enum  Variant {X, x, O, o};
-    Variant noteX[2] = { X, x };
-    Variant noteO[2] = { O, o };
-    Variant NumberOne = x;
-    std::cout << NumberOne <<std::endl;
-    unsigned char Zerous[3][3] = {{x,X,o},{O,o,x},{O,x,o}}; // Вроде прописал нужный тип данных, но выводятся не те символы, что мне нужны. Плюс, наврное, надо результаты перечисления предоставлять как возможные для ввода игроком во время работы приложения и подставлять в ячейки массива? Если да, то пока не понимаю, как сделать.
-    std::cout << "| " << Zerous[0][0] << " | " << Zerous[0][1] << " | " << Zerous[0][2] << " | \n" << "-------------\n" << "| " << Zerous[1][0] << " | " << Zerous[1][1] << " | " << Zerous[1][2] << " | \n" << "-------------\n" << "| " << Zerous[2][0] << " | " << Zerous[2][1] << " | " << Zerous[2][2] << " | \n";
+    enum  MyGame { x, o};
+        MyGame noteX[1] = { x };
+        MyGame noteO[1] = { o };
+        MyGame NumberOne = x;
+        MyGame NumberTwo = o;
+        std::cout << NumberOne <<std::endl;
+        std::cout << NumberTwo << std::endl;
+    
+    MyGame arr[3][3] = { 0 }; 
+        std::cout << "| " << arr[0][0] << " | " << arr[0][1] << " | " << arr[0][2] << " | \n" << "-------------\n" << "| " << arr[1][0] << " | " << arr[1][1] << " | " << arr[1][2] << " | \n" << "-------------\n" << "| " << arr[2][0] << " | " << arr[2][1] << " | " << arr[2][2] << " | \n";
+        arr[0][0] = NumberOne;
+        arr[0][1] = NumberTwo;
+
+    Game v1, v2;
+        v1.size = 3;
+        v1.difficulty = 31;
+        v1.points = 120'000.0;
+        v2 = { 5, 28, 750'000.0 };
+
+     MyVariant info;
+        info.BotIq = 25;
+        std::cout << info.BotIq << std::endl; // Выведет на экран: 25
+        info.MyPin = 'm';
+        std::cout << info.MyPin << std::endl; // Выведет на экран: m
+        info.MyMultiplier = 3.75;
+        std::cout << info.MyMultiplier << std::endl; // Выведет на экран: 3.75
+        
+        
+        
+        
     
     return 0;
 }
